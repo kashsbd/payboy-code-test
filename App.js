2b8c436f367c3f5334191b0848bfa8adff058b1e
Kash <parakash.sbd@gmail.com>
2022-07-01 21:08:56 +0630

    bottom tab bar is done

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Main from "./src/main";
@@ -13,11 +12,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/main.js
+++ src/main.js
@@ -1,14 +1,39 @@
-import * as React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeScreen from './screen/homeScreen';
-import CreateUserScreen from './screen/createUserScreen';
+import CreateUserScreen from "./screen/createUserScreen";
+import HomeScreen from "./screen/homeScreen";
 
 const Tab = createBottomTabNavigator();
 
 function Main() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return (
+              <Ionicons
+                name={focused ? "md-home-sharp" : "md-home-outline"}
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === "Create User") {
+            return (
+              <Ionicons
+                name={focused ? "reorder-three-sharp" : "reorder-three-outline"}
+                size={35}
+                color={color}
+              />
+            );
+          }
+        },
+        tabBarInactiveTintColor: "gray",
+        tabBarActiveTintColor: "black",
+        tabBarLabel: ({ route }) => null,
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Create User" component={CreateUserScreen} />
     </Tab.Navigator>
