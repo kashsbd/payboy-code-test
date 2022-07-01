7122a4072e9931d188115f44e1d7366759adc2de
Kash <parakash.sbd@gmail.com>
2022-07-01 21:21:18 +0630

    done setting up react-native-paper

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
 
 import Main from "./src/main";
+import store from "./src/redux/store";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <Main />
-    </NavigationContainer>
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <Main />
+        </NavigationContainer>
+      </PaperProvider>
+    </StoreProvider>
   );
 }
-
